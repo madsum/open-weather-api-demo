5454d326938cdb3645c54fec1647520f3caeb3e0
Muhammed Masum Islam <madsum@gmail.com>
2021-12-05 19:16:18 +0200

    Add generated springboot project

---
+++ src/main/java/com/example/openweatherapidemo/OpenWeatherApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.openweatherapidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenWeatherApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenWeatherApiDemoApplication.class, args);
+	}
+
+}
