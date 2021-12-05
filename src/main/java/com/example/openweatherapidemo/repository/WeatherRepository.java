8857ed5de561d631ba38693f4d0e74153b3c0e05
Muhammed Masum Islam <madsum@gmail.com>
2021-12-05 19:52:24 +0200

    Implement WeatherRepository

---
+++ src/main/java/com/example/openweatherapidemo/repository/WeatherRepository.java
@@ -0,0 +1,18 @@
+package com.example.openweatherapidemo.repository;
+
+import com.example.openweatherapidemo.model.Weather;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface WeatherRepository extends JpaRepository<Weather, Long> {
+
+    @Query("SELECT w FROM Weather w WHERE w.city LIKE :city")
+    Optional<List<Weather>> findAllByCity(@Param("city") String city);
+
+}
